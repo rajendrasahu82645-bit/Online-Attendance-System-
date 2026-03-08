6c568bc164fd6e84705c0774f502d1fe405ec250
Rajendra Sahu <rajendrasahu82645@gmail.com>
2026-03-08 22:34:09 +0530

    Fix import and configure app to use environment port
    Fix import statement and update app run configuration to use environment port.

--- apps.py
+++ apps.py
@@ -1,4 +1,5 @@
 frome flask import flask
+import os
 
 app =flask(__name__)
 
@@ -7,4 +8,5 @@ def home():
     return "server running successfully"
 
 if __name__== "__main__":
-    app.run()
+    port =int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0",port=port)
